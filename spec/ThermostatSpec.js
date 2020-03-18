d6ae4a0c0a7ba3a7aacfbd9438bdf34258a7540d
WilliamJGrace <williamjgrace@outlook.com>
2020-03-18 14:47:45 +0000

    set a minimum temperature

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -1,9 +1,39 @@
 describe ('Thermostat', function(){
-  it("Thermostat starts at 20 degress", function(){
+
+  var thermostat
+
+  beforeEach(function(){
     thermostat = new Thermostat();
+  });
+
+
+  it("Thermostat starts at 20 degress", function(){
     expect(thermostat.getCurrentTemperature()).toEqual(20);
+  });
 
-
+  it("Can increase the temparature", function(){
+    thermostat.up()
+    expect(thermostat.getCurrentTemperature()).toEqual(21)
 
   });
+
+  it("Can decrease the temparature", function(){
+    thermostat.down()
+    expect(thermostat.getCurrentTemperature()).toEqual(19)
+
+  });
+
+  it("has a minimum of 10 degrees", function(){
+    for(var i=0; i<11; i++){
+      thermostat.down();
+    }
+    expect(thermostat.getCurrentTemperature()).toEqual(10)
+
+
+    
+  });
+
+
+
+
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -2,10 +2,25 @@
 
 function Thermostat(){
   this.temperature = 20;
+  this.MINIMUM_TEMPERATURE = 10
+
 
 };
 
 Thermostat.prototype.getCurrentTemperature = function () {
   return this.temperature;
+};
 
+Thermostat.prototype.up = function () {
+  this.temperature++
+};
+
+Thermostat.prototype.down = function () {
+  if(this.isMinimumTemperature()){
+    return;
+  }
+  this.temperature--
+};
+Thermostat.prototype.isMinimumTemperature = function () {
+  return this.temperature === this.MINIMUM_TEMPERATURE;
 };
